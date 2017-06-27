c12725c64133e468703b9e4483a1d5583ffd147d
rdgdemelo <rms10@cin.ufpe.br>
2017-06-27 16:31:21 -0300

    Producer & Consumer update

--- src/app/Consumer.java
+++ src/app/Consumer.java
@@ -4,15 +4,29 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 
 import distribution.QueueManagerProxy;
+import distribution.message.Operation;
 
 public class Consumer {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		
-		QueueManagerProxy queue1proxy = new QueueManagerProxy("topic1");
+		QueueManagerProxy proxy1 = new QueueManagerProxy("queue1");
+		QueueManagerProxy proxy2 = new QueueManagerProxy("queue2");
+		
+		proxy1.send("topic1", "", Operation.SUBSCRIBE);
+		proxy1.send("topic2", "", Operation.SUBSCRIBE);
+		
+		proxy2.send("topic11", "", Operation.SUBSCRIBE);
+		proxy2.send("topic22", "", Operation.SUBSCRIBE);
+		
+		proxy1.send("topic3", "", Operation.SUBSCRIBE);
 		
 		while (true) {
-			System.out.println(queue1proxy.receive());
+			//Fila 1
+			System.out.println(proxy1.receive());
+			
+			//Fila 2
+			System.out.println(proxy2.receive());
 		}
 		
 	}
