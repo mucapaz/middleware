13d624130a8d2f29ac50590b900d59110b7c4598
Samuel Paz <pazspm@gmail.com>
2017-07-05 17:12:57 -0300

    Deleting some producers and consumers

--- src/app/Consumer2.java
+++ src/app/Consumer2.java
@@ -2,22 +2,61 @@ package app;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.Set;
 
 import distribution.QueueManagerProxy;
+import distribution.message.Message;
 import distribution.message.Operation;
 
-public class Consumer2 {
+public class Consumer2 implements Runnable{
 
-	public static void main(String[] args) throws UnknownHostException, IOException {
+	private QueueManagerProxy proxy;
+	private String name;
+	
+	public Consumer2(String name, QueueManagerProxy proxy) {
+		this.proxy = proxy;
+		this.name = name;
+	}
+	
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+		
+		QueueManagerProxy proxy1 = new QueueManagerProxy();	
+		QueueManagerProxy proxy2 = new QueueManagerProxy();
+	
+		proxy1.send("topic1", "", Operation.SUBSCRIBE);
+		
+		proxy1.send("topic2", "", Operation.SUBSCRIBE);
+		
+		proxy2.send("topic11", "", Operation.SUBSCRIBE);
+		proxy2.send("topic22", "", Operation.SUBSCRIBE);
 
-		QueueManagerProxy proxy1 = new QueueManagerProxy();
+		proxy1.send("topic3", "", Operation.SUBSCRIBE);
 		
 		proxy1.send("topic1", "", Operation.SUBSCRIBE);
 		
-		while (true) {
-			
-			System.out.println(proxy1.receive());
-		}
+		Consumer2 c1 = new Consumer2("Consumer 1", proxy1);
+		
+		Consumer2 c2 = new Consumer2("Consumer 2", proxy2);
+		
+		
+		Thread t1 = new Thread(c1);
+		Thread t2 = new Thread(c2);
+		
+		
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
+	
 	}
 
+	@Override
+	public void run() {
+		while (true) {
+			Message msg = proxy.receive();
+			
+			System.out.println(name + " -> "  + msg.getPayload().getContent());				
+		}
+	}
 }
--- src/distribution/QueueManager.java
+++ src/distribution/QueueManager.java
@@ -18,52 +18,52 @@ import repositories.TopicSubscriberMapRepository;
 public class QueueManager implements Runnable{
 
 	public static int port = Config.queueManagerPort;
-	
+
 	private HashMap<String, List<Integer>> topicSubscribersMap;
-	
+
 	int count = 0;
-	
+
 	private Queue queue;
-	
+
 	private ServerRequestHandler serverHandler;
-	
+
 	public static void main(String[] args) throws IOException, InterruptedException{
-		
+
 		QueueManager queue = new QueueManager();
 		Thread t = new Thread(queue);
 		t.start();
-		
+
 		Scanner in = new Scanner(System.in);
-		
+
 		while(in.hasNextLine()){
 			String str = in.nextLine();
-			
+
 			if(str.equals("exit")){
 				System.exit(0);
 			}		
 		}
-		
+
 	}
-	
+
 	public QueueManager() throws IOException{	
-		
+
 		queue = new Queue();
-		
+
 		topicSubscribersMap = TopicSubscriberMapRepository.readFromDisk();
-		
+
 		//restoreQueueTopics();
-		
+
 		serverHandler = new ServerRequestHandler(port, this);
-		
+
 		Runtime.getRuntime().addShutdownHook(new Thread(){
 			public void run(){
 				System.out.println("Running shutdown Hook at QueueManager.java");
 				TopicSubscriberMapRepository.saveToDisk(topicSubscribersMap);
 			}
 		});
-		
+
 	}
-	
+
 	public void restoreQueueTopics(){
 		//Cadastra os tópicos presentes na fila do disco 
 		Iterator<Message> it = queue.getQueue().iterator();
@@ -74,75 +74,76 @@ public class QueueManager implements Runnable{
 
 	public void run(){
 		MessagePassThread messagePassThread;
-		
+
 		messagePassThread = new MessagePassThread(this,!this.queue.isEmpty());
-		
+
 		Thread thread = new Thread(messagePassThread);
 		thread.start();
-		
+
 		while(true){
 			serverHandler.connect();
-			
+
 			System.out.println("QueueManager -> serverHandler.connect()");	
 		}
 	}
-	
+
 	public synchronized void message(int connectionId, Message msg) {
-		
+
 		Header header = msg.getHeader();
 		Operation operation = header.getOperation();
-			
+
 		if(operation.equals(Operation.PUBLISH)){
 			queue.enqueue(msg);
-			
+
 			String topic = msg.getHeader().getTopic();
-			
+
 			if(!topicSubscribersMap.containsKey(topic)){
 				topicSubscribersMap.put(topic, new ArrayList<Integer>());
 			}
-			
+
 		}else if(operation.equals(Operation.SUBSCRIBE)){
 			subscribe(connectionId, msg.getHeader().getTopic());
-			
+
 		}else if (operation.equals(Operation.LIST)){
 			try {
 				Message message  = MessageCreator.createMessage("",  topicSubscribersMap.keySet().toArray() ,Operation.LIST); 
-							
+
 				serverHandler.send(connectionId, message);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	private void subscribe(int subscriber, String topic) {
-		
+
 		if(!topicSubscribersMap.containsKey(topic))
 			topicSubscribersMap.put(topic, new ArrayList<Integer>());
-			
+
 		topicSubscribersMap.get(topic).add(subscriber);		
 	}
 
 	protected void publish(Message msg) {
-		
-		if(Config.persistanceTest)
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
+
+		if(Config.persistanceTest){
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
 		}
-		
+
 		String topic = msg.getHeader().getTopic();
-		
+
 		List<Integer> subs = topicSubscribersMap.get(topic); //pega subs
-		
+
 		if(subs!=null){
 			for(Iterator<Integer> i = subs.iterator(); i.hasNext();){
 				int sub = i.next();
 				try {
 					serverHandler.send(sub, msg);
 				} catch (IOException e) {
-					
+
 				}catch(NullPointerException npe){
 					i.remove();
 					System.out.println("Removed Offline Subscriber from Topic: " + topic);
@@ -150,13 +151,13 @@ public class QueueManager implements Runnable{
 			}
 		}
 	}
-	
+
 	private synchronized void removeSubscribers(List<Integer> remove, List<Integer> subs){
 		for(Integer at : remove){
 			subs.remove(at);
 		}
 	}
-	
+
 	public void updateConnectionId(int oldId, int cookieId){
 		serverHandler.updateConnectionId(oldId, cookieId);
 	}
@@ -164,5 +165,5 @@ public class QueueManager implements Runnable{
 	public Message dequeue() {	
 		return queue.dequeue();
 	}
-	
+
 }
